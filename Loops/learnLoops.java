f99b25a3a8bfbf4f71215fe1fd8bc179a466b99e
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-05-26 03:22:32 +0545

    modified files

--- Loops/learnLoops.java
+++ Loops/learnLoops.java
@@ -1,2 +1,11 @@
-package Loops;public class learnLoops {
+package Loops;
+
+public class learnLoops {
+    public static void main(String[] args) {
+        //print your name 10 times
+
+        for(int i=0; i<10; i++){
+            System.out.println("My name is Dipak Bhandari.");
+        }
+    }
 }
--- Loops/mulTableOfAnyNum.java
+++ Loops/mulTableOfAnyNum.java
@@ -1,2 +1,18 @@
-package Loops;public class mulTableOfAnyNum {
+package Loops;
+
+import java.util.Scanner;
+
+public class mulTableOfAnyNum {
+
+    //This is to print multiplication tables of given number up to 10
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the num : ");
+        int a = sc.nextInt();
+        for(int i=1; i<= 10; i++){  // i=1; i<=10; (from 1 up to 10)
+
+            System.out.println(a+" *" +i +" = " +i*a); //print 'a' * 'i' = a*i
+        }
+    }
 }
