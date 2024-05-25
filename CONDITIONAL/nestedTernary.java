ccdf541f44e9d7da678a2fb71bbc7addd7eef383
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-05-26 01:45:49 +0545

    modified files

--- CONDITIONAL/nestedTernary.java
+++ CONDITIONAL/nestedTernary.java
@@ -1,2 +1,24 @@
-package CONDITIONAL;public class nestedTernary {
+package CONDITIONAL;
+
+public class nestedTernary {
+
+    public static void main(String[] args) {
+        // Initialize three numbers to check which is the largest/smallest
+
+        int a = 634;
+        int b = 543;
+        int c = 742;
+
+        //initialize a variable to store result
+        int max;
+
+        //now ternary
+        // first check if a is greatest of all if yes print a otherwise c
+        // then b is checked if yes print b otherwise c
+
+        max= a>b? a>c? a:c :b>c? b:c;
+
+        System.out.println("The largest among them : "+max);
+
+    }
 }
--- SwitchStatementsDemo/switchDemo.java
+++ SwitchStatementsDemo/switchDemo.java
@@ -1,2 +1,24 @@
-package SwitchStatementsDemo;public class switchDemo {
+package SwitchStatementsDemo;
+import java.util.*;
+public class switchDemo {
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your choice");
+
+        int a= sc.nextInt();
+        switch (a){
+            case 1:
+                System.out.println("HAHA");
+                break;
+
+            case 2:
+                System.out.println("OH la la");
+                break;
+
+            default:
+                System.out.println("Invalid");
+
+        }
+    }
 }
