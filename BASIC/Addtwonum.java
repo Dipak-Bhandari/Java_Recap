0fb75814861e29a563eb5527c5fe5a94e08cf2ec
Dipak Bhandari <101723901+Dipak-Bhandari@users.noreply.github.com>
2024-05-25 18:12:52 +0545

    Add files via upload

---
+++ BASIC/Addtwonum.java
@@ -0,0 +1,12 @@
+package BASIC;
+
+public class Addtwonum {
+    public static void main(String[] args) {
+
+        int a=66;
+        int b=55;
+        int sum= a + b;
+        System.out.println(sum);
+
+    }
+}
