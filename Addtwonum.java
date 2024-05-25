28bd15c203dd32febb4c3388ee8de2acd2e4e0cf
Dipak Bhandari <101723901+RenderWanderrer@users.noreply.github.com>
2024-05-25 16:43:20 +0545

    Add files via upload

---
+++ Addtwonum.java
@@ -0,0 +1,10 @@
+public class Addtwonum {
+    public static void main(String[] args) {
+
+        int a=66;
+        int b=55;
+        int sum= a + b;
+        System.out.println(sum);
+
+    }
+}
---
+++ variablesDataTypes.java
@@ -0,0 +1,34 @@
+public class variablesDataTypes {
+    public static void main(String[] args) {
+        int a=55555555;
+        float b=4.555f;
+        char myChar= 'A';
+        boolean isTrue = false;
+        double secondB=3.14;
+        short s= 124;
+        long l= 8539845;
+        String  str= "happy";
+
+
+        // Implicite DataType Conversion = directly convert any small datatype into larger datatype
+        // int - long, int - string
+
+        int count= 13;
+        long countLong = count;
+
+        //print "countLong" to access "count"
+        System.out.println(countLong);
+
+        // Explicit datatype conversion
+        long sum = 90;
+        int sumLong= (int)sum;
+        //print the int variable "sumLong" to access "sum"
+        System.out.println(sumLong);
+
+        int age=150;
+        byte newAge= (byte)age; //Type CASTING/ TYPE CONVERSION
+        //PRINT
+        System.out.println(newAge);
+
+    }
+}
