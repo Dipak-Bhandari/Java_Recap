a5733ae4a86da6b72c74de22a6fb3d0d4d0143ac
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-05-28 23:04:20 +0545

    modified files

--- Loops/doWhileLoops.java
+++ Loops/doWhileLoops.java
@@ -1,2 +1,19 @@
-package Loops;public class doWhileLoops {
+package Loops;
+
+import java.util.Scanner;
+
+public class doWhileLoops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean isUnderstood = false;
+        do{
+            System.out.println("Goto School");
+            System.out.println("Have you understood ?");
+            isUnderstood = sc.nextBoolean();
+        }
+        while (!isUnderstood);
+        System.out.println("Good JOB !!");
+    }
 }
+// used in cases where you need to at least once have to execute a loop
+// Syntax: do{ body }while(condition);
