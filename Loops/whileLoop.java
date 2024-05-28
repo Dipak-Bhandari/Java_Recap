52e8b15c2ed5545c937227bc7190d999d40507e6
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-05-28 20:58:26 +0545

    added files

---
+++ Loops/whileLoop.java
@@ -0,0 +1,9 @@
+package Loops;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        // while loops is used when we know when a loop is going to stop/end
+        // while(noend){sout(keep going)}
+
+    }
+}
