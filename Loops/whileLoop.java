da8064e68eb145c10bdd907386e6007907db08a1
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-05-28 22:50:21 +0545

    added files

--- Loops/whileLoop.java
+++ Loops/whileLoop.java
@@ -1,9 +1,22 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class whileLoop {
     public static void main(String[] args) {
         // while loops is used when we know when a loop is going to stop/end
         // while(noend){sout(keep going)}
-
+//        int a =0;
+//        while(a<5){
+//            System.out.println(a);
+//            a++;
+//                   }
+        Scanner sc = new Scanner(System.in);
+        boolean isUnderstood =false ;
+        while(!isUnderstood){
+            System.out.println("goto school");
+            System.out.println("Did You understand ? ");
+            isUnderstood = sc.nextBoolean();
+        }
     }
 }
