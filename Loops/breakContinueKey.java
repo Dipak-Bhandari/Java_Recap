c274dd00ad85d72705dc7ea8e7ec31be586393e8
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-05-29 18:49:14 +0545

    modified files

--- Loops/breakContinueKey.java
+++ Loops/breakContinueKey.java
@@ -1,2 +1,43 @@
-package Loops;public class breakContinueKey {
+package Loops;
+
+public class breakContinueKey {
+    public static void main(String[] args) {
+
+//        for (int i = 0; i <=20 ; i++) {         // prints up to 20
+//            System.out.println(i);              //  prints value of i
+//            if(i>=15)break;                     // prints only numbers that satisfies condition
+//        }
+// also same for while/do-while loops
+//        int a=0;
+//        while(a<=5){
+//           System.out.println(a);         // while(condition) {
+//           a++;                           //  code
+//           if(a==3)break;                 //  if(condition to break){ break;} }
+//        }
+
+//
+//        int b=0;
+//            do{
+//                if(b>4)break;
+//                System.out.println(b);
+//                b++;
+//            }while(b<8);
+
+
+//
+//        for (int i = 0; i <=20 ; i++) {
+//            if(i==15) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+        int i = 0;
+        do{
+            if(i==4) continue;
+            System.out.println(i);
+            i++;
+        }while (i<=6);
+    }
 }
+
+
