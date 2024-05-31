6ae8ab211f68f1cec21d5eb1bc68812cf8cdb93e
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-05-31 22:52:10 +0545

    modified files

--- BASIC/finalKey.java
+++ BASIC/finalKey.java
@@ -1,2 +1,10 @@
-package BASIC;public class finalKey {
+package BASIC;
+
+public class finalKey {
+    public static void main(String[] args) {
+        final double Pi=3.14;
+        // final is used to define constants
+        // Pi =4; it gives error as constants can't be redefines/changed
+        System.out.println(Pi);
+    }
 }
