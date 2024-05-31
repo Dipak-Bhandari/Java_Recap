a052f78f66c6258359912dbf476613b6c198ce73
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-05-31 22:53:22 +0545

    modified files

---
+++ useArrays/FindingMiinArray.java
@@ -0,0 +1,16 @@
+package useArrays;
+
+import java.util.Scanner;
+
+public class FindingMiinArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array Length: ");
+        int length = sc.nextInt();
+        int numbers[] =new int[length];
+        System.out.println("Enter the numbers for array: ");
+
+
+
+    }
+}
