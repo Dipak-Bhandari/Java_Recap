1ab0fd78c3ef7dcd22bda5281b637569abafc1f1
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-05-31 22:52:48 +0545

    modified files

--- Loops/labeledBreakContinuw.java
+++ Loops/labeledBreakContinuw.java
@@ -1,2 +1,25 @@
-package Loops;public class labeledBreakContinuw {
+package Loops;
+
+public class labeledBreakContinuw {
+    public static void main(String[] args) {
+        // label is a name given to a loop so that it can be called in order to perform specific actions
+
+        //outerLoop: //this is a label
+
+        for (int i = 1; i <=5 ; i++) {
+            innerLoop:
+            for (int j = i; j <=5; j++) {
+
+                if(j==2)continue innerLoop;
+
+                System.out.print(j+" ");
+
+            }
+            System.out.println();
+
+            // it is not a recommended practice as it makes code unreadable.
+
+            // label is used to terminate loops inside loops(nested loop)
+        }
+    }
 }
