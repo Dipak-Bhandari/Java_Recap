3b590f3134d45e2d4beb0ee6d6d1a1c468aaf10a
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-02 16:51:58 +0545

    modified files

--- Basic2/Operators.java
+++ Basic2/Operators.java
@@ -2,14 +2,14 @@ package Basic2;
 
 public class Operators {
     public static void main(String[] args) {
-      // ARITHMETIC OPERATORS +,-,*,/,%
-        int a = 13;
-        int b = 5;
-        int c = a+b;
-        int d = a-b;
-        int e = a*b;
-        double f= (double)a/b;
-        int g = a%b;
+      // ARITHMETIC OPERATORS +,-,*,/,%  performs simple arithmetic operations
+//        int a = 13;
+//        int b = 5;
+//        int c = a+b;
+//        int d = a-b;
+//        int e = a*b;
+//        double f= (double)a/b;
+//        int g = a%b;
 
 //        System.out.println(c);
 //        System.out.println(d);
@@ -17,22 +17,20 @@ public class Operators {
 //        System.out.println(f);
 //        System.out.println(g);
 
-        // ASSIGNMENT OPERATOR
-//        int oneNum=11;
-//        int secNum=43;
-//        System.out.println(oneNum==secNum);
-//        System.out.println(oneNum!=secNum);
-//        System.out.println(oneNum>secNum);
-//        System.out.println(oneNum<secNum);
-//        System.out.println(oneNum>=secNum);
-//        System.out.println(oneNum<=secNum);
+        // Relational OPERATOR  operators used to check relations between two operands
+        int oneNum=11;
+        int secNum=43;
+        System.out.println(oneNum==secNum);
+        System.out.println(oneNum!=secNum);
+        System.out.println(oneNum>secNum);
+        System.out.println(oneNum<secNum);
+        System.out.println(oneNum>=secNum);
+        System.out.println(oneNum<=secNum);
 
-
-        // LOGICAL OPERATORS
+        // LOGICAL OPERATORS similar to gate it performs logical AND, OR, NOT
 
 //        boolean firstexpn=true;
 //         boolean secondexpn=false;
-//
 //        System.out.println(firstexpn && secondexpn);
 //
 //        System.out.println(firstexpn || secondexpn);
