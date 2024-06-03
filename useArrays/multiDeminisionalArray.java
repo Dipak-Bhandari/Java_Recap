ed740cd651fd2d5358f15f2e7199dd761dd50db5
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-03 22:46:20 +0545

    new file

--- useArrays/multiDeminisionalArray.java
+++ useArrays/multiDeminisionalArray.java
@@ -1,32 +1,36 @@
 package useArrays;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class multiDeminisionalArray {
-    // 2D Array
-    // in initialization we have to declare the size of that array too
+    public static void main(String args[]) {
 
-   // int marks[][] = new int[4][3];  // initialization if 2D Array
 
-//    marks[0][0]=65; // Declaration of 2D Array
-//    marks[0][1]=65;
-//    marks[0][2]=65;
+        // 2D Array
+        // in initialization we have to declare the size of that array too
+
+    int marks[][] = new int[3][4];  // initialization if 2D Array
+
+    marks[0][0]=65; // Declaration of 2D Array
+    marks[0][1]=65;
+    marks[0][2]=65;
+
+    marks[1][0]=65;
+    marks[1][1]=65;
+    marks[1][2]=65;
+
+    marks[2][0]=65;
+    marks[2][1]=65;
+    marks[2][2]=65;
+
+        // another way
+//    int marks[][]= {
+//            {27,52,64},
+//            {27,65,64},
+//            {12,65,44},
+//            {12,52,44}
 //
-//    marks[1][0]=65;
-//    marks[1][1]=65;
-//    marks[1][2]=65;
-//
-//    marks[2][0]=65;
-//    marks[2][1]=65;
-//    marks[2][2]=65;
+//    };
 
-    // another way
-    int marks[][]= {
-            {27,52,64},
-            {27,65,64},
-            {12,65,44},
-            {12,52,44}
-
-    };
-
-
-
+    }
 }
