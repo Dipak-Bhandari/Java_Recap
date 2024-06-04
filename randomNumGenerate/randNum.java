5b142d1870a3af9816f3d60334ba856aaffa91a1
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-04 14:01:52 +0545

    new file

---
+++ randomNumGenerate/randNum.java
@@ -0,0 +1,14 @@
+package randomNumGenerate;
+
+class randNum {
+    public static void main( String[] args ) {
+// using math.Random() to generate random numbers
+
+        int min= 1;
+        int max=6;
+         int random_Num = (int)Math.floor(Math.random()*(max-min+1)+min);
+
+        System.out.println("The random number is : "+random_Num);
+
+    }
+}
