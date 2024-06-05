e2202eb8e04ecb85569b32c07f872c1d5dc04740
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-05 23:21:34 +0545

    new file

--- useArrays/ReverseArray.java
+++ useArrays/ReverseArray.java
@@ -2,6 +2,24 @@ package useArrays;
 
 public class ReverseArray {
     public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int[] reversedArray = reverseArray(array);
 
+        System.out.println("Reversed array:");
+        for (int i : reversedArray) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] reverseArray(int[] array) {
+        int length = array.length;
+        int[] reversedArray = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            reversedArray[i] = array[length - 1 - i];
+        }
+
+        return reversedArray;
     }
 }
+
