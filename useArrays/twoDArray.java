6332a2ce877c382fb9c49d394f937ec1d9950321
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-06 15:14:55 +0545

    new file

--- useArrays/twoDArray.java
+++ useArrays/twoDArray.java
@@ -1,2 +1,20 @@
-package useArrays;public class twoDArray {
+package useArrays;
+
+public class twoDArray {
+    public static void main(String[] args) {
+
+        // initialization and allocation of 2-D Array
+        int[][] arr={
+                {1,2,5,4},
+                {14,7,87,6},
+                {44,9,3,45}
+        };
+        // nested for loop to print
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j < arr.length ; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
 }
