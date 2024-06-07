d38fb855f8efd92dc9dcff88f6e98cb118625cac
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-07 21:11:31 +0545

    modified  file

--- SwitchStatementsDemo/VowelOrConsonant.java
+++ SwitchStatementsDemo/VowelOrConsonant.java
@@ -1,2 +1,42 @@
-package SwitchStatementsDemo;public class VowelOrConsonant {
+package SwitchStatementsDemo;
+
+import java.util.Scanner;
+
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter an alphabet : ");
+        char alpha = sc.next().charAt(0);
+
+        if ((alpha>='A' && alpha<= 'z') ||( alpha>='a'&& alpha<='z'))
+
+        {
+            switch (alpha){
+
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'O':
+                case 'U':
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+
+                    System.out.println("It is a Vowel.");
+                    break;
+
+                default:
+                    System.out.println("It is a Consonant.");
+            }
+
+
+        }
+        else{
+            System.out.println("Not an alphabet dumb!!");
+
+        }
+
+    }
 }
