3d40d99efadb2032166da4e6c97e80a99b3f91af
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-15 23:49:16 +0545

    string initialization

---
+++ Strings/initializeStr.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class initializeStr {
+    public static void main(String[] args) {
+        // strings can be initialized in following ways:
+        //1. String literal
+
+        String str = "My name";
+        System.out.println(str);
+
+
+        //2. using new keyword
+         String strName = new String("My real name");
+        System.out.println(strName);
+
+
+    }
+}
