54df981ce757c64016248544f5ccb4b082c56c03
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-19 18:09:52 +0545

    various math inbuilt functions  file

---
+++ funcMethods/mathPreBuiltFunc.java
@@ -0,0 +1,27 @@
+package funcMethods;
+
+public class mathPreBuiltFunc {
+    public static void main(String[] args) {
+
+        // returns minimum value
+        System.out.println("the min value : "+Math.min(4,5));
+
+        // returns max value
+        System.out.println("max value: "+Math.max(10,8));
+
+        // returns absolute value it changes sign from -ve to +ve
+        System.out.println("absolute value : "+Math.abs(3.4));
+        System.out.println("-ve to +ve : "+Math.abs(-99));
+
+        //returns power/exponential value
+        System.out.println("Power of 3^2 :"+Math.pow(3,2));
+
+        //returns square root
+        System.out.println("square root of 81 : "+Math.sqrt(81));
+
+        // returns floor value (5.8)-> 5
+        System.out.println("floor value : "+Math.floor(5.8));
+
+
+    }
+}
