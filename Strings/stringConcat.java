e2d23095294e6a7579dbd593120e9bd49658ead3
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-20 23:12:55 +0545

    string cocat  file

---
+++ Strings/stringConcat.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class stringConcat {
+    public static void main(String[] args) {
+        String firstName = " Hello";
+        String lastName = " world";
+
+        //basically adding two string
+        String conCat = firstName+lastName;
+        System.out.println(conCat);
+    }
+}
