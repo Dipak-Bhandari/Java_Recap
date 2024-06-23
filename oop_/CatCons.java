65eda4f2b614734d159b9ae5cb4b098a573c48b8
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-23 23:43:21 +0545

    constructor in java

---
+++ oop_/CatCons.java
@@ -0,0 +1,47 @@
+package oop_;
+
+public class CatCons {
+    // Instance Variables
+    String name;
+    String breed;
+    int age;
+    String color;
+
+    // Constructor Declaration of Class
+    public CatCons(String name, String breed, int age,
+               String color)
+    {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+
+    // method 1
+    public String getName() { return name; }
+
+    // method 2
+    public String getBreed() { return breed; }
+
+    // method 3
+    public int getAge() { return age; }
+
+    // method 4
+    public String getColor() { return color; }
+
+    @Override public String toString()
+    {
+        return ("Hi, this is my dog  " + this.getName()
+                + ".\n it's  breed,age and color are : "
+                + this.getBreed() + "," + this.getAge()
+                + "," + this.getColor());
+    }
+
+    public static void main(String[] args)
+    {
+        CatCons tuffy
+                = new CatCons("Bob", "Persian", 5, "white");
+        System.out.println(tuffy.toString());
+    }
+}
+
