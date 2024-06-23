57118f446c8366ad78cc462bb8fd5173bd0fed71
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-23 23:00:34 +0545

    method overloading file

--- oop_/MethodOverload.java
+++ oop_/MethodOverload.java
@@ -2,13 +2,16 @@ package oop_;
 
 //two or more methods can have same name inside same class
 // if they take different arguments
+//it can be achieved : 1. changing number of arguments
+//2.changing the data type of argument
 public class MethodOverload {
     public static void main(String[] args) {
-
+        //creating greet object
         Greet gg = new Greet();
+        //calling function
         gg.greetings();
         gg.greetings("Dipu");
-
+        gg.greetings("Rohit",5);
     }
 }
 class Greet{
@@ -21,5 +24,9 @@ class Greet{
 
         System.out.println("Hello "+name+", Good morning!");
     }
-
+    void greetings(String name, int count){
+        for (int i = 0; i <count ; i++) {
+            System.out.println("Hello "+name+", Good morning!");
+        }
+    }
 }
