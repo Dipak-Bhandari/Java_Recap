94083b8326e112c1dc24b492e7dca5cb232dd960
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-25 23:09:07 +0545

    inheritance in java

--- opps1/Car.java
+++ opps1/Car.java
@@ -1,13 +1,16 @@
 package opps1;
-
+//inherits vehicle class
 public class Car extends Vehicle {
 
     public static void main(String[] args) {
+        //object initialized
         Car car = new Car();
+        //parameters passed
         car.WheelsCount=4;
         car.color="Cream";
         car.plateNo="B_kha-3_4141";
         car.model="Volvo";
+        //calling function
         car.start();
 
 
