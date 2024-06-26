8d75e5963b6bba4bfe60608e075a7192ea668638
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-26 22:34:49 +0545

    super keyword in java

--- SuperKey/Animal.java
+++ SuperKey/Animal.java
@@ -1,4 +1,16 @@
 package SuperKey;
 
 public class Animal {
+
+    int legs;
+    String name;
+    String breed;
+
+    Animal(){
+        System.out.println("Creating an animal ");
+
+    }
+    Animal(int legsCount){
+        System.out.println("Animal with legs is being created");
+    }
 }
--- SuperKey/Cat.java
+++ SuperKey/Cat.java
@@ -1,4 +1,17 @@
 package SuperKey;
 
 public class Cat {
+
+    String name;
+    String breed;
+    int legs;
+    public static void main(String[] args) {
+
+    Cat cat = new Cat();
+    cat.legs=4;
+    cat.name="Billi";
+    cat.breed= "eastern";
+
+    }
+
 }
