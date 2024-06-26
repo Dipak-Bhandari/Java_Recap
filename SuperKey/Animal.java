822ed43af7130a695285033b41a3cf7a8fb0fcfb
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-26 22:58:15 +0545

    super keyword in java

--- SuperKey/Animal.java
+++ SuperKey/Animal.java
@@ -1,11 +1,12 @@
 package SuperKey;
 
 public class Animal {
-
+//default constructor
     Animal(){
         System.out.println("Creating an animal ");
 
     }
+    //creating a function
     void noise(){
 
         System.out.println("Animal is making noise.");
--- SuperKey/Cat.java
+++ SuperKey/Cat.java
@@ -1,12 +1,13 @@
 package SuperKey;
-
+//inherits properties of animal class
 public class Cat extends Animal {
-
+//properties of cat class
     String name;
     String breed;
     int legs;
-
+//function of cat class
 void noise(){
+    //looking at parent implementation using super keyword
     super.noise();
     System.out.println("Cat is Purring");
 }
@@ -14,13 +15,14 @@ void noise(){
 void mew(){
     System.out.println("Cat is mewing.");
 }
-    public static void main(String[] args) {
-
+// main method
+public static void main(String[] args) {
+//cat instance
     Cat cat = new Cat();
     cat.legs=4;
     cat.name="Billi";
     cat.breed= "eastern";
-    cat.noise();
+    cat.noise();        //calling functions
     cat.mew();
     System.out.println("Cat named "+cat.name+" is "+cat.breed+" and has "+cat.legs+" legs.");
     }
