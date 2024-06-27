2c29a1e90af4a089c2bddc91c9693b7dd5fa8d7f
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-27 23:10:19 +0545

    Encapsulation in java

--- oops2/Encapsulation/MainEncapsule.java
+++ oops2/Encapsulation/MainEncapsule.java
@@ -1,4 +1,16 @@
 package oops2.Encapsulation;
 
 public class MainEncapsule {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name= "Mukesh";
+        System.out.println(person.name);
+
+        // person.age = 34; // this gives error because it's private
+        //calling setter method to set age
+        person.setAge(34);
+        //calling getter method to print age
+        System.out.println(person.getAge());
+
+    }
 }
--- oops2/Encapsulation/Person.java
+++ oops2/Encapsulation/Person.java
@@ -1,4 +1,19 @@
 package oops2.Encapsulation;
 
 public class Person {
+    //access modifiers used for data hiding and encapsulation
+    private int age ;
+
+    public String name;
+
+    //method to set age
+    public void setAge(int age){
+        this.age= age;
+    }
+    //returns age
+    public int getAge(){
+        return age;
+    }
+
+
 }
