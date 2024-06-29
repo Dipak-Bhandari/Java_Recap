9864d5c46a62c99ccbfa76f44a633662ed789a7b
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-29 22:17:41 +0545

    abstraction in java

--- oops3/AbstractDemo.java
+++ oops3/AbstractDemo.java
@@ -1,7 +1,39 @@
 package oops3;
 
 public class AbstractDemo {
-}
-class Vehicle{
 
+    public static void main(String[] args) {
+//abstract object cannot be created
+//        Vehicle vehicle = new Vehicle();
+      //Creating obj of Car class
+        Car car = new Car();
+        //calling methods
+        car.accelerate();
+        car.brakes();
+
+    }
+}
+
+
+abstract class Vehicle{
+    //abstract method doesn't have a body
+    abstract void accelerate();
+
+    abstract void brakes();
+
+}
+// normal class inheriting from abstract class must implement abstract methods
+class Car extends Vehicle{
+
+// @ is an annotation that checks if method overridden exists in parent classes
+    @Override
+    void accelerate() {
+        System.out.println("Car is accelerating.");
+    }
+
+    @Override
+    void brakes() {
+        System.out.println("Car's breaks are pushed.");
+
+    }
 }
