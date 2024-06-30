6195b0d9cdde3d5f66143354bad0b0475ab7049e
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-30 22:39:24 +0545

    abstraction and interface in java

---
+++ oops3/InterfacesDemo.java
@@ -0,0 +1,11 @@
+package oops3;
+
+//abstraction can be achieved via interfaces too
+//with the help of interfaces we can achieve multiple inheritance in java
+interface Animal{
+    void eats();
+
+}
+public class InterfacesDemo {
+}
+
