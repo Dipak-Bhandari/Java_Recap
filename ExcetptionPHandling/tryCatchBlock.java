249601c8c29a3cb9a5d32c74929f512f5142f1aa
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-07-02 23:40:16 +0545

    Divided by zero exception java

--- ExcetptionPHandling/tryCatchBlock.java
+++ ExcetptionPHandling/tryCatchBlock.java
@@ -1,5 +1,20 @@
 package ExcetptionPHandling;
 
+//try catch is a basic exception handling method
+//inside try block you give the exception that might occur
+//inside catch block you catch exception and print the occurred exception
 public class tryCatchBlock {
+
+    public static void main(String[] args) {
+         try {
+             int a = 5;
+             int b = 0;
+             int ans = a/b;
+
+         }
+         catch (Exception e ){
+             System.out.println("Exception : "+e.getMessage());
+         }
+    }
 
 }
