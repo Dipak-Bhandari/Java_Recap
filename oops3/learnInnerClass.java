bca8dce00b83bc7761214532d66f8b368c7ae3b5
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-07-03 20:04:49 +0545

    Inner class and nested static class in java

--- oops3/learnInnerClass.java
+++ oops3/learnInnerClass.java
@@ -3,33 +3,37 @@ package oops3;
 
 public class learnInnerClass {
 
-    //Non-static class within another class
-    //it has access to members of outer class
+//Non-static class within another class
+//it has access to members of outer class
     class Inner{
-
         int price;
         String text;
     }
 
+//creating a static nested class
     static class staticNested{
         String text2;
     }
+//main method
     public static void main(String[] args) {
 
-//        creating an object of non-static inner class
-        //first way create object of outer class and then create object of non-static class
+//creating an object of non-static inner class
+//first way create object of outer class and then create object of non-static class
         learnInnerClass obj = new learnInnerClass();
         Inner inner  =obj. new Inner();
 
-//        2nd way creating object of both classes at same time
-//        Inner inner1 = new learnInnerClass(). new Inner();
-
+// 2nd way creating object of both classes at same time
+//Inner inner1 = new learnInnerClass(). new Inner();
 
 //creating object of static nested class
         staticNested cc = new learnInnerClass.staticNested();
 
+//assigning variable its value
+        inner.price=45;
         inner.text= "Hello, non-static";
         cc.text2 ="Hello, static";
+//printing the values
+        System.out.println(inner.price);
         System.out.println(inner.text);
         System.out.println(cc.text2);
     }
